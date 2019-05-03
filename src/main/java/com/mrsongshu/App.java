225ef6e1e1712152744d47a66aaabd7078cf88bc
xhskedu <xhskedu@qq.com>
2019-05-03 10:53:40 +0000

    replace with new edition

---
+++ src/main/java/com/mrsongshu/App.java
@@ -0,0 +1,13 @@
+package com.mrsongshu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
